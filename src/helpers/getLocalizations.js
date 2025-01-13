bf30fec1ab2ecad16f9141cd36fc3e59cc5969f3
Thomas Franz <thomas.franz94@googlemail.com>
2025-01-13 14:43:02 +0100

    improve localizations code

--- src/helpers/getLocalizations.js
+++ src/helpers/getLocalizations.js
@@ -8,34 +8,33 @@ exports.default = (SERVER_ROOT, TRANSLATES_PATH) => {
     const translates = [];
 
     translateFiles.forEach(translateFile => {
-    const fileUrl = TRANSLATES_PATH + translateFile;
-    const lang = path.basename(fileUrl, `.json`);
-    const content = JSON.parse(fs.readFileSync(fileUrl, `utf8`));
+        const fileUrl = TRANSLATES_PATH + translateFile;
+        const lang = path.basename(fileUrl, `.json`);
+        const content = JSON.parse(fs.readFileSync(fileUrl, `utf8`));
+        const isEnglish = lang === 'en';
+        const insertionMethod = isEnglish ? 'unshift' : 'push';
 
-    if (lang === `en`) {
-        // EN needs a different path and also has to be in first place of array
-        languages.unshift({
-        name: content.langName,
-        code: null,
-        });
-    
-        translates.unshift({
-        content: content,
-        dest: SERVER_ROOT,
-        path: `.`
-        })
-    } else {
-        languages.push({
-        name: content.langName,
-        code: lang,
-        });
-    
-        translates.push({
-        content: content,
-        dest: SERVER_ROOT + lang,
-        path: `..`
-        })
-    }
+        const language_data = {
+            name: content.langName,
+            code: lang,
+        };
+
+        const content_data = {
+            content: content,
+            dest: SERVER_ROOT + lang,
+            path: `..`
+        }
+
+        if (isEnglish) {
+            // EN needs a different path and also has to be in first place of array
+            language_data['code'] = null;
+            content_data['dest'] = SERVER_ROOT;
+            content_data['path'] = `.`;
+        } 
+            
+        languages[insertionMethod](language_data);
+        translates[insertionMethod](content_data)
+
     });
 
     return {languages, translates}
